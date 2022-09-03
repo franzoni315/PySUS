e1e61d60060c9d413105cc71a5b5c7d0075c4acc
franzoni315 <franzoni315@gmail.com>
2022-09-03 15:28:15 -0300

    removing geopandas

--- pysus/utilities/readdbc.py
+++ pysus/utilities/readdbc.py
@@ -10,7 +10,6 @@ from tqdm import tqdm
 from io import BytesIO
 from tempfile import NamedTemporaryFile
 
-import geopandas as gpd
 import pandas as pd
 from dbfread import DBF
 
@@ -18,30 +17,6 @@ try:
     from pysus.utilities._readdbc import ffi, lib
 except (ImportError, ModuleNotFoundError):
     from _readdbc import ffi, lib
-
-
-def read_dbc(filename, encoding="utf-8", raw=False):
-    """
-    Opens a DATASUS .dbc file and return its contents as a pandas
-    Dataframe.
-    :param filename: .dbc filename
-    :param encoding: encoding of the data
-    :param raw: Skip type conversion. Set it to True to avoid type conversion errors
-    :return: Pandas Dataframe.
-    """
-    if isinstance(filename, str):
-        filename = filename.encode()
-    with NamedTemporaryFile(delete=False) as tf:
-        dbc2dbf(filename, tf.name.encode())
-        try:
-            dbf = DBF(tf.name, encoding=encoding, raw=raw)
-            df = gpd.GeoDataFrame(list(dbf))
-        except ValueError as exc:
-            dbf = DBF(tf.name, encoding=encoding, raw=not raw)
-            df = gpd.GeoDataFrame(list(dbf))
-    os.unlink(tf.name)
-
-    return df
 
 
 def dbc2dbf(infile, outfile):
@@ -61,35 +36,6 @@ def dbc2dbf(infile, outfile):
 
     # print(os.path.exists(outfile))
 
-
-def read_dbc_geopandas(filename, encoding="utf-8"):
-    """
-    Opens a DATASUS .dbc file and return its contents as a pandas
-    Dataframe, using geopandas
-    :param filename: .dbc filename
-    :param encoding: encoding of the data
-    :return: Pandas Dataframe.
-    """
-    if isinstance(filename, str):
-        filename = filename
-    with NamedTemporaryFile(delete=False) as tf:
-        out = tf.name + ".dbf"
-        dbc2dbf(filename, out)
-        dbf = gpd.read_file(out, encoding=encoding).drop("geometry", axis=1)
-        df = pd.DataFrame(dbf)
-        os.unlink(out)
-    os.unlink(tf.name)
-
-    return df
-
-def read_dbc_dbf(filename: str):
-    if filename.endswith(('dbc', 'DBC')):
-        df = read_dbc(filename, encoding="iso-8859-1")
-    elif filename.endswith(("DBF", "dbf")):
-            # dbf = DBF(filename, encoding="iso-8859-1")
-            dbf = gpd.read_file(filename, encoding="iso-8859-1").drop("geometry", axis=1)
-            df = pd.DataFrame(list(dbf))
-    return df
 
 def dbf_to_csvgz(filename: str, encoding: str='iso-8859-1'):
     """
